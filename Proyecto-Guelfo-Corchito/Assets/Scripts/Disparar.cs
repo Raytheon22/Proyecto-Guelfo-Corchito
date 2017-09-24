dae7dd26b08a8b351df145af567ffd31387e167e
DESKTOP-IN949M0\sebas <sebastianromeo1@hotmail.com>
2017-09-24 18:43:13 -0300

    Ahora si el Cochito funciona para celular, la ver anterior no la habia subido :V, falta arreglar la animacion de la botella  del memotest

--- Proyecto-Guelfo-Corchito/Assets/Scripts/Disparar.cs
+++ Proyecto-Guelfo-Corchito/Assets/Scripts/Disparar.cs
@@ -8,8 +8,11 @@ public class Disparar : MonoBehaviour {
 	public GameObject manager;
     public bool Cargado = true;
     public float Velocidad;
+    public bool CargarVel = false;
+    public bool Lanzar;
+    public bool Recargar = false;
 
-	void Awake () 
+    void Awake () 
 	{
         Velocidad = 0;
 	}
@@ -19,26 +22,28 @@ public class Disparar : MonoBehaviour {
 
 
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) || CargarVel)
         {
             Velocidad += 0.2f;
         }
 
-        if (Input.GetKeyUp(KeyCode.Space) && Cargado)
+        if (Input.GetKeyUp(KeyCode.Space) && Cargado || Lanzar && Cargado)
         {
+            CargarVel = false;
             GameObject proyectil = Instantiate(Corcho) as GameObject;
             proyectil.transform.position = transform.position + transform.forward * Time.deltaTime;
 			proyectil.transform.rotation = Random.rotation;
             Rigidbody rig = proyectil.GetComponent<Rigidbody>();
             rig.velocity = transform.forward * Velocidad;
 			Cargado = false;
-
+            Lanzar = false;
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space) || Recargar)
         {
             Velocidad = 0;
             Cargado = true;
+            Recargar = false;
         }
 	}
 }
---
+++ Proyecto-Guelfo-Corchito/Assets/Scripts/Tap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour {
+
+    public GameObject Jugador;
+
+    void Awake () {
+
+	}
+
+    void OnMouseDown()
+    {
+        Jugador.GetComponent<Disparar>().CargarVel = true;
+    }
+
+    void OnMouseUp()
+    {
+        Jugador.GetComponent<Disparar>().CargarVel = false;
+        Jugador.GetComponent<Disparar>().Lanzar = true;
+        Jugador.GetComponent<Disparar>().Recargar = true;
+    }
+
+    void Update()
+    {
+
+    }
+
+}
+
+
+
